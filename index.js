5e64f5fcf97d43afd4cf4949b3c2400cf0a78243
yael <yaelabadlia4@gmail.com>
2025-09-01 14:09:15 +0200

    routage

--- index.js
+++ index.js
@@ -3,32 +3,79 @@ import 'dotenv/config'
 
 const app = express()
 
-const m1 = (req, res, next) => {
-    console.log("Middleware : m1");
-    next()
-}
-
-const m2 = (req, res, next) => {
-    console.log("Middleware : m2");
-    next()
-}
-
-const m3 = (req, res) => {
-    console.log("Middleware : m3");
-}
-
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
     console.log("GET : /")
     res.end("GET : /")
-    next()
+
 })
-// }, [m2, m1])
-// app.use(m1)
+app.route(['/home', '/accueil'])
+    .get((req, res) => {
+        console.log(`GET : ${req.url}`)
+        // res.end(`GET : ${req.url}`)
+        res
+        .type("html")
+        .sendFile(import.meta.dirname + '/index.html')
+        // next()
+    })
+    .post((req, res) => {
+        console.log(`POST : ${req.url}`)
+        res.end(`POST : ${req.url}`)
+        // next()
+    })
 
 
-// app.use(m2)
+app.all("/personne", (req, res) => {
+    console.log(`${req.method} : ${req.url}`)
+    res.end(`${req.method} : ${req.url}`)
+})
 
-app.use([m1, m3, m2])
+app.get('/adresse', (req, res) => {
+    res.end(`Ici c'est ${req.query['ville']} - ${req.query.cp}`)
+
+})
+app.get('/adresse/:ville/:cp', (req, res) => {
+    res.end(`Ici c'est ${req.params['ville']} - ${req.params.cp}`)
+
+})
+// app.get(['/home', '/accueil'], (req, res) => {
+//     console.log(`GET : ${req.url}`)
+//     res.end(`GET : ${req.url}`)
+//     // next()
+// })
+// app.post(['/home', '/accueil'], (req, res) => {
+//     console.log(`POST : ${req.url}`)
+//     res.end(`POST : ${req.url}`)
+//     // next()
+// })
+
+app.get('/calcul/:op', (req, res) => {
+    const { a, b } = req.query
+    switch (req.params.op) {
+        case 'plus': res.end(`${a} + ${b} = ${Number(a) + Number(b)}`); break;
+        case 'moins': res.end(`${a} - ${b} = ${Number(a) - Number(b)}`); break;
+        case 'fois': res.end(`${a} * ${b} = ${Number(a) * Number(b)}`); break;
+        case 'div': res.end(`${a} / ${b} = ${Number(a) / Number(b)}`); break;
+        default: res.end(`L'opérateur ${req.params.op} est inconnu`)
+    }
+})
+
+// middleware pour les routes restantes : à placer en dernier
+app.get('/*splat', (req, res) => {
+    console.log("GET : La route demandée n'existe pas")
+    res
+        .status(404)
+        .type("json")
+        .json({
+            "Erreur": "La page demandée n'existe pas"
+        })
+    // res
+    //     .sendStatus(404)
+    // res
+    //     .status(404)
+    //     .end("GET : La route demandée n'existe pas")
+
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {

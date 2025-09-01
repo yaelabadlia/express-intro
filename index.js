90e88e9cf4ad220a60f00f33bbecbb8083e02462
yael <yaelabadlia4@gmail.com>
2025-09-01 11:17:01 +0200

    middleware

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-// import http from 'node:http'
-
-// const server = http.createServer((req, res) => {
-//     res.end("Hello world!")
-// })
 import express from 'express'
 import 'dotenv/config'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.end("GET : /")
-})
-app.post('/', (req, res) => {
-    res.end("POST : /")
-})
-app.get('/personne', (req, res) => {
-    res.end("GET : /personne")
-})
-app.get('/formation', (req, res) => {
-    res.end("GET : /formation")
-})
+const m1 = (req, res, next) => {
+    console.log("Middleware : m1");
+    next()
+}
 
+const m2 = (req, res, next) => {
+    console.log("Middleware : m2");
+    next()
+}
+
+const m3 = (req, res) => {
+    console.log("Middleware : m3");
+}
+
+app.get('/', (req, res, next) => {
+    console.log("GET : /")
+    res.end("GET : /")
+    next()
+})
+// }, [m2, m1])
+// app.use(m1)
+
+
+// app.use(m2)
+
+app.use([m1, m3, m2])
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
